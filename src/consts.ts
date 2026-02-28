3cbd9eb94b014d5b061f4ff9abeb2e677c11c5e9
austinspringer64 <austinspringer64@gmail.com>
2026-02-27 18:46:51 -0700

    codex changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Austin Springer';
+export const SITE_DESCRIPTION = 'Personal blog on software, open source, and projects.';
+export const SITE_AUTHOR = 'Austin Springer';
+export const SOCIAL_LINKS = {
+	github: 'https://github.com/austinspringer',
+	linkedin: 'https://www.linkedin.com/in/austinspringer/',
+};
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,8 @@
+---
+title: 'Hello, World!'
+description: 'My first blog post!'
+pubDate: 'Feb 27 2026'
+heroImage: '/blog-placeholder-2.jpg'
+---
+
+This is my first blog post! I'm excited to start sharing my thoughts and experiences with you.
